4cca2a6d6dd43448c01ea278e3448ecddbd72647
ideaa <ideaa@qq.com>
2025-04-19 16:24:32 +0800

    WithMsg

--- ws/w_response.go
+++ ws/w_response.go
@@ -29,6 +29,11 @@ func (r *Response) WithError(e Error, err error) *Response {
 	return r
 }
 
+func (r *Response) WithMsg(msg string) *Response {
+	r.Ctx.Msg = msg
+	return r
+}
+
 func (r *Response) Send() {
 	r.g.JSON(r.httpCode, r.Ctx)
 }
