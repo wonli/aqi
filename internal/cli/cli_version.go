c3c6201834a9bb35b86bfa8de8e0cd079c23cbf6
ideaa <ideaa@qq.com>
2026-09-04 09:54:04 +0800

    refactor(cli): derive version from build info

--- internal/cli/cli_version.go
+++ internal/cli/cli_version.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"fmt"
+	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,8 +12,41 @@ var verCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Version of this CLI",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Version: 2.0.1")
+		fmt.Println("Version:", version())
 	},
+}
+
+func version() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return strings.TrimPrefix(info.Main.Version, "v")
+	}
+
+	var revision string
+	var modified bool
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			revision = setting.Value
+		case "vcs.modified":
+			modified = setting.Value == "true"
+		}
+	}
+
+	if len(revision) > 7 {
+		revision = revision[:7]
+	}
+	if revision == "" {
+		return "devel"
+	}
+	if modified {
+		return revision + "-dirty"
+	}
+	return revision
 }
 
 func init() {
