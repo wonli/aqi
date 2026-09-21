4731b806a14e9468c4b7489fb26c84b243e2ac88
ideaa <ideaa@qq.com>
2026-09-21 19:25:58 +0800

    refactor: remove cluster test hooks from production

--- ws/cluster.go
+++ ws/cluster.go
@@ -114,27 +114,6 @@ func InitClusterTransport(factory ClusterTransportFactory) error {
 	return nil
 }
 
-// setClusterTransport is a compact test helper for installing fake transports.
-func setClusterTransport(t ClusterTransport) {
-	clearClusterTransport()
-	if t == nil {
-		return
-	}
-	instanceID, err := newClusterInstanceID()
-	if err != nil {
-		panic("aqi: cannot generate cluster instance id: " + err.Error())
-	}
-	if !installClusterTransport(t, instanceID) {
-		panic("aqi: cannot install test cluster transport")
-	}
-}
-
-func clearClusterTransport() {
-	if old := clusterState.Swap(nil); old != nil {
-		_ = old.transport.Close()
-	}
-}
-
 func clusterAcquire(topic string) {
 	c := clusterState.Load()
 	if topic == "" || c == nil {
